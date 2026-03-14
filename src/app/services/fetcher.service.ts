e2c12e0716843c33bdae5ebfd8d2fc18b2a781ff
Chumr26 <nguyenanhkhoa26092002@gmail.com>
2026-03-14 17:59:46 +0700

    mirate from json-server to firebase realtime db

--- src/app/services/fetcher.service.ts
+++ src/app/services/fetcher.service.ts
@@ -3,34 +3,35 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class FetcherService {
-  private apiUrl = 'http://localhost:3000/';
-  constructor(private http: HttpClient) {}
+    // TODO: Replace this with your actual Firebase Realtime Database URL
+    private apiUrl = 'https://furniture-9e350-default-rtdb.asia-southeast1.firebasedatabase.app/';
+    constructor(private http: HttpClient) { }
 
-  // Generic GET request
-  get<T>(endpoint: string): Observable<T> {
-    return this.http.get<T>(this.apiUrl + endpoint);
-  }
+    // Generic GET request
+    get<T>(endpoint: string): Observable<T> {
+        return this.http.get<T>(this.apiUrl + endpoint + '.json');
+    }
 
-  // Generic POST request
-  post<T>(endpoint: string, body: T): Observable<T> {
-    return this.http.post<T>(this.apiUrl + endpoint, body);
-  }
+    // Generic POST request
+    post<T>(endpoint: string, body: T): Observable<T> {
+        return this.http.post<T>(this.apiUrl + endpoint + '.json', body);
+    }
 
-  // Generic PUT request
-  put<T>(endpoint: string, body: T): Observable<T> {
-    return this.http.put<T>(this.apiUrl + endpoint, body);
-  }
+    // Generic PUT request
+    put<T>(endpoint: string, body: T): Observable<T> {
+        return this.http.put<T>(this.apiUrl + endpoint + '.json', body);
+    }
 
-  // Generic PATCH request
-  patch<T>(endpoint: string, body: Partial<T>): Observable<T> {
-    return this.http.patch<T>(this.apiUrl + endpoint, body);
-  }
+    // Generic PATCH request
+    patch<T>(endpoint: string, body: Partial<T>): Observable<T> {
+        return this.http.patch<T>(this.apiUrl + endpoint + '.json', body);
+    }
 
-  // Generic DELETE request
-  delete<T>(endpoint: string): Observable<T> {
-    return this.http.delete<T>(this.apiUrl + endpoint);
-  }
+    // Generic DELETE request
+    delete<T>(endpoint: string): Observable<T> {
+        return this.http.delete<T>(this.apiUrl + endpoint + '.json');
+    }
 }
